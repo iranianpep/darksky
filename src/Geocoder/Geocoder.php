10decdcac898ec2c4857084440d086ff089441b6
Ehsan Abbasi <iranianpep@gmail.com>
2017-06-27 23:06:08 +1000

    Add more tests

--- src/Geocoder/Geocoder.php
+++ src/Geocoder/Geocoder.php
@@ -6,11 +6,56 @@ class Geocoder
 {
     const API_URL = 'http://maps.google.com/maps/api/geocode';
 
+    private $rawResponse;
+    private $defaultOutputFormat = 'json';
+
+    /**
+     * @return string
+     */
+    public function getDefaultOutputFormat(): string
+    {
+        return $this->defaultOutputFormat;
+    }
+
+    /**
+     * @param string $defaultOutputFormat
+     */
+    public function setDefaultOutputFormat(string $defaultOutputFormat)
+    {
+        $this->defaultOutputFormat = $defaultOutputFormat;
+    }
+
     public function geocode($address)
     {
+        $outputFormat = $this->getDefaultOutputFormat();
         $address = urlencode($address);
-        $json = file_get_contents(self::API_URL."/json?address={$address}&sensor=false");
+        $json = file_get_contents(self::API_URL."/{$outputFormat}?address={$address}&sensor=false");
 
-        return json_decode($json, true);
+        $this->rawResponse = $json;
+
+        return $this;
+    }
+
+    public function asRaw()
+    {
+        return $this->rawResponse;
+    }
+
+    public function asArray()
+    {
+        if ($this->getDefaultOutputFormat() !== 'json') {
+            throw new \Exception('This function is only compatible with json as default output format');
+        }
+
+        return json_decode($this->rawResponse, true);
+    }
+
+    public function asObject()
+    {
+        if ($this->getDefaultOutputFormat() !== 'json') {
+            throw new \Exception('This function is only compatible with json as default output format');
+        }
+
+        return json_decode($this->rawResponse);
     }
 }
--- tests/Geocoder/GeocoderTest.php
+++ tests/Geocoder/GeocoderTest.php
@@ -6,79 +6,82 @@ use PHPUnit\Framework\TestCase;
 
 class GeocoderTest extends TestCase
 {
-    public function testGeocode()
+    public function testAsRaw()
     {
-        $geocoder = new Geocoder();
+        $this->assertEquals(
+            $this->getExpectedData(),
+            (new Geocoder())->geocode('Melbourne, Australia')->asRaw()
+        );
+    }
 
-        $expected = [
-            'results' => [
-                [
-                    'address_components' => [
-                            [
-                                'long_name'  => 'Melbourne',
-                                'short_name' => 'Melbourne',
-                                'types'      => [
-                                        0 => 'colloquial_area',
-                                        1 => 'locality',
-                                        2 => 'political',
-                                    ],
-                            ],
-                            [
-                                'long_name'  => 'Victoria',
-                                'short_name' => 'VIC',
-                                'types'      => [
-                                        0 => 'administrative_area_level_1',
-                                        1 => 'political',
-                                    ],
-                            ],
-                            [
-                                'long_name'  => 'Australia',
-                                'short_name' => 'AU',
-                                'types'      => [
-                                        0 => 'country',
-                                        1 => 'political',
-                                    ],
-                            ],
-                        ],
-                    'formatted_address' => 'Melbourne VIC, Australia',
-                    'geometry'          => [
-                            'bounds' => [
-                                    'northeast' => [
-                                            'lat' => -37.51127369999999672245394322089850902557373046875,
-                                            'lng' => 145.512528800000012552118278108537197113037109375,
-                                        ],
-                                    'southwest' => [
-                                            'lat' => -38.43385930000000172412910615094006061553955078125,
-                                            'lng' => 144.593741800000003649984137155115604400634765625,
-                                        ],
-                                ],
-                            'location' => [
-                                    'lat' => -37.81362759999999667570591554976999759674072265625,
-                                    'lng' => 144.96305760000001328080543316900730133056640625,
-                                ],
-                            'location_type' => 'APPROXIMATE',
-                            'viewport'      => [
-                                    'northeast' => [
-                                            'lat' => -37.51127369999999672245394322089850902557373046875,
-                                            'lng' => 145.512528800000012552118278108537197113037109375,
-                                        ],
-                                    'southwest' => [
-                                            'lat' => -38.43385930000000172412910615094006061553955078125,
-                                            'lng' => 144.593741800000003649984137155115604400634765625,
-                                        ],
-                                ],
-                        ],
-                    'place_id' => 'ChIJ90260rVG1moRkM2MIXVWBAQ',
-                    'types'    => [
-                            'colloquial_area',
-                            'locality',
-                            'political',
-                        ],
-                ],
-            ],
-            'status' => 'OK',
-        ];
+    public function testAsArray()
+    {
+        $expected = json_decode($this->getExpectedData(), true);
+        $this->assertEquals($expected, (new Geocoder())->geocode('Melbourne, Australia')->asArray());
+    }
 
-        $this->assertEquals($expected, $geocoder->geocode('Melbourne, Australia'));
+    public function testAsObject()
+    {
+        $expected = json_decode($this->getExpectedData());
+        $this->assertEquals($expected, (new Geocoder())->geocode('Melbourne, Australia')->asObject());
+    }
+
+    private function getExpectedData()
+    {
+        return '{
+   "results" : [
+      {
+         "address_components" : [
+            {
+               "long_name" : "Melbourne",
+               "short_name" : "Melbourne",
+               "types" : [ "colloquial_area", "locality", "political" ]
+            },
+            {
+               "long_name" : "Victoria",
+               "short_name" : "VIC",
+               "types" : [ "administrative_area_level_1", "political" ]
+            },
+            {
+               "long_name" : "Australia",
+               "short_name" : "AU",
+               "types" : [ "country", "political" ]
+            }
+         ],
+         "formatted_address" : "Melbourne VIC, Australia",
+         "geometry" : {
+            "bounds" : {
+               "northeast" : {
+                  "lat" : -37.5112737,
+                  "lng" : 145.5125288
+               },
+               "southwest" : {
+                  "lat" : -38.4338593,
+                  "lng" : 144.5937418
+               }
+            },
+            "location" : {
+               "lat" : -37.8136276,
+               "lng" : 144.9630576
+            },
+            "location_type" : "APPROXIMATE",
+            "viewport" : {
+               "northeast" : {
+                  "lat" : -37.5112737,
+                  "lng" : 145.5125288
+               },
+               "southwest" : {
+                  "lat" : -38.4338593,
+                  "lng" : 144.5937418
+               }
+            }
+         },
+         "place_id" : "ChIJ90260rVG1moRkM2MIXVWBAQ",
+         "types" : [ "colloquial_area", "locality", "political" ]
+      }
+   ],
+   "status" : "OK"
+}
+';
     }
 }
