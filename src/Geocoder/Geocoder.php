9b22603874204420f687b1a3cf2e3fbf901a444c
Ehsan Abbasi <iranianpep@gmail.com>
2017-07-01 12:47:10 +1000

    Add region

--- src/Geocoder/Geocoder.php
+++ src/Geocoder/Geocoder.php
@@ -5,18 +5,23 @@ namespace Geocoder;
 class Geocoder
 {
     const API_URL = 'http://maps.google.com/maps/api/geocode';
+    const VALID_OUTPUT_FORMAT = ['json', 'xml'];
 
     /**
      * @param        $address
+     * @param string $region
      * @param string $outputFormat
      *
      * @return string
+     * @throws \Exception
      */
-    public function geocode($address, $outputFormat = 'json')
+    public function geocode($address, $region = '', $outputFormat = 'json')
     {
-        return file_get_contents(
-            self::API_URL.'/'.$outputFormat.'?address='.urlencode($address).'&sensor=false'
-        );
+        if ($this->validateOutputFormat($outputFormat) !== true) {
+            throw new \Exception("'{$outputFormat}' is not a valid format.");
+        }
+
+        return file_get_contents($this->generateRequestUrl($address, $region, $outputFormat));
     }
 
     /**
@@ -42,5 +47,38 @@ class Geocoder
         }
 
         return $latLng;
+    }
+
+    /**
+     * @param $format
+     *
+     * @return bool
+     */
+    private function validateOutputFormat($format)
+    {
+        if (in_array($format, self::VALID_OUTPUT_FORMAT)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @param        $address
+     * @param string $region
+     * @param string $outputFormat
+     * @param bool   $sensor
+     *
+     * @return string
+     */
+    private function generateRequestUrl($address, $region = '', $outputFormat = 'json', $sensor = false)
+    {
+        $baseUrl = self::API_URL.'/'.$outputFormat.'?address='.urlencode($address).'&sensor='.$sensor;
+
+        if (!empty($region)) {
+            $baseUrl .= "&region={$region}";
+        }
+
+        return $baseUrl;
     }
 }
